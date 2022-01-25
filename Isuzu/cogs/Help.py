69857d7acaf2288662f8c2f3e2d7a9ba3f1276b1
Ace <47049298+ramareds18@users.noreply.github.com>
2022-01-26 03:18:59 +0700

    timeout and untimeout help changed

--- Isuzu/cogs/Help.py
+++ Isuzu/cogs/Help.py
@@ -242,21 +242,19 @@ class Help(commands.Cog):
 
         await ctx.reply(embed = em, mention_author = False)
 
-    @help.command(aliases=['to'])
+    @help.command()
     async def timeout(self, ctx):
         em = discord.Embed(title = '**timeout**', description = "Timeout a user for a set duration in the server. Reason is optional.\n**NOTE:** This will DM the user.", colour=0xf00000, timestamp = pen.now('Asia/Jakarta'))
-        em.add_field(name = 'Aliases', value = '`to`', inline = False)
         em.add_field(name = 'Duration options', value='• w - Week\n• d - Day\n• h - Hour\n• m - Minute\n• s - Second', inline = False)
-        em.add_field(name = 'Usage and Examples', value = '`timeout <userID> <duration> <reason>`\n```\ntimeout 302064098739355652 10d14h34m2s stinky```', inline = False)
+        em.add_field(name = 'Usage and Examples', value = 'Slash command, type /timeout.\n**Duration examples:**\n```\n10d14h34m2s\n1h4s```', inline = False)
         em.set_footer(text = f"{ctx.author.display_name} ({ctx.author.id})", icon_url = ctx.author.display_avatar)
 
         await ctx.reply(embed = em, mention_author = False)
 
-    @help.command(aliases=['uto'])
+    @help.command()
     async def untimeout(self, ctx):
         em = discord.Embed(title = '**timeout**', description = "Remove timeout from a user in the server. Reason is optional.\n**NOTE:** This will DM the user.", colour=0xf00000, timestamp = pen.now('Asia/Jakarta'))
-        em.add_field(name = 'Aliases', value = '`uto`', inline = False)
-        em.add_field(name = 'Usage and Examples', value = '`untimeout <userID> <reason>`\n```\nuntimeout 302064098739355652 not stinky anymore```', inline = False)
+        em.add_field(name = 'Usage and Examples', value = '`Slash command, type /untimeout.`', inline = False)
         em.set_footer(text = f"{ctx.author.display_name} ({ctx.author.id})", icon_url = ctx.author.display_avatar)
 
         await ctx.reply(embed = em, mention_author = False)
