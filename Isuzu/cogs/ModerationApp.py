6b095d721dd75df82086d8135e5f69d89054fa70
Ace <47049298+ramareds18@users.noreply.github.com>
2022-01-30 16:06:47 +0700

    added lock and unlock channel + change cog name

--- Isuzu/cogs/ModerationApp.py
+++ Isuzu/cogs/ModerationApp.py
@@ -7,7 +7,7 @@ from nextcord.errors import Forbidden
 from pendulum import datetime as dt
 
 
-class ModerationSlash(commands.Cog):
+class ModerationApp(commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -204,5 +204,71 @@ class ModerationSlash(commands.Cog):
         else:
             await interaction.response.send_message("I don't have `Timeout Members` permission.")
 
+    @discord.slash_command(name="lock", description="Lockdown a channel")
+    async def lock_slash(
+        self,
+        interaction: Interaction, 
+        channel: GuildChannel = SlashOption(
+            name = "channel",
+            description = "Channel to lock",
+            required = False,
+            channel_types = [ChannelType.text],
+        )
+    ):
+        if interaction.user.guild_permissions.manage_roles and interaction.user.guild.me.guild_permissions.manage_messages:
+            reason = f'Locked down by {interaction.user.name}#{interaction.user.discriminator} ({interaction.user.id})'
+            if not channel:
+                overwrites = interaction.channel.overwrites_for(interaction.guild.default_role)
+                if overwrites.send_messages != False:
+                    await interaction.channel.set_permissions(interaction.guild.me, send_messages = True, reason = reason)
+                    await interaction.channel.set_permissions(interaction.guild.default_role, send_messages = False, reason = reason)
+                    await interaction.response.send_message(f"Locked {interaction.channel.mention}.")
+                else:
+                    await interaction.response.send_message(f"This channel is already locked.")
+            else:
+                overwrites = channel.overwrites_for(interaction.guild.default_role)
+                if overwrites.send_messages != False:
+                    await channel.set_permissions(interaction.guild.me, send_messages = True, reason = reason)
+                    await channel.set_permissions(interaction.guild.default_role, send_messages = False, reason = reason)
+                    await interaction.response.send_message(f"Locked {channel.mention}.")
+                else:
+                    await interaction.response.send_message(f"That channel is already locked.")
+        elif not interaction.user.guild.me.guild_permissions.manage_messages:
+            await interaction.response.send_message("You don't have `Manage Messages` permission.", ephemeral=True)
+        else:
+            await interaction.response.send_message("I don't have `Manage Roles` permission.")
+
+    @discord.slash_command(name="unlock", description="Unlock a locked channel")
+    async def unlock_slash(
+        self,
+        interaction: Interaction, 
+        channel: GuildChannel = SlashOption(
+            name = "channel",
+            description = "Channel to unlock",
+            required = False,
+            channel_types = [ChannelType.text],
+        )
+    ):
+        if interaction.user.guild_permissions.manage_roles and interaction.user.guild.me.guild_permissions.manage_messages:
+            reason = f'Lockdown removed by {interaction.user.name}#{interaction.user.discriminator} ({interaction.user.id})'
+            if not channel:
+                overwrites = interaction.channel.overwrites_for(interaction.guild.default_role)
+                if overwrites.send_messages == False:
+                    await interaction.channel.set_permissions(interaction.guild.default_role, send_messages = None, reason = reason)
+                    await interaction.response.send_message(f"Unlocked {interaction.channel.mention}.")
+                else:
+                    await interaction.response.send_message(f"This channel is not locked.")
+            else:
+                overwrites = channel.overwrites_for(interaction.guild.default_role)
+                if overwrites.send_messages == False:
+                    await channel.set_permissions(interaction.guild.default_role, send_messages = None, reason = reason)
+                    await interaction.response.send_message(f"Unlocked {channel.mention}.")
+                else:
+                    await interaction.response.send_message(f"That channel is not locked.")
+        elif not interaction.user.guild.me.guild_permissions.manage_messages:
+            await interaction.response.send_message("You don't have `Manage Messages` permission.", ephemeral=True)
+        else:
+            await interaction.response.send_message("I don't have `Manage Roles` permission.")            
+            
 def setup(client):
-    client.add_cog(ModerationSlash(client))
+    client.add_cog(ModerationApp(client))
